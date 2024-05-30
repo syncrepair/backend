7a03c7cba3acd41e4a394079118eda03f97d6f65
Yaroslav <xxehwuq@gmail.com>
2024-05-31 01:23:37 +0200

    removed zerolog logger

--- cmd/main.go
+++ cmd/main.go
@@ -4,7 +4,6 @@ import (
 	"github.com/labstack/echo/v4"
 	"github.com/syncrepair/backend/internal/config"
 	"github.com/syncrepair/backend/internal/handler"
-	"github.com/syncrepair/backend/internal/logger"
 	"github.com/syncrepair/backend/internal/repository"
 	"github.com/syncrepair/backend/internal/usecase"
 	"github.com/syncrepair/backend/pkg/database/mongo"
@@ -13,44 +12,14 @@ import (
 const configFilePath = "config.yml"
 
 func main() {
-	// Configuration
 	cfg := config.Load(configFilePath)
-
-	// Logging
-	l := logger.New(cfg.LogLevel)
-
-	l.Info().
-		Str("version", cfg.App.Version).
-		Msgf("🚀 Starting %s", cfg.App.Name)
-
-	// Databases
-	l.Info().
-		Msg("Connecting to mongo database")
 
 	mongoClient := mongo.NewClient(cfg.Mongo.URI)
 	mongoDB := mongoClient.Database(cfg.Mongo.Name)
 
-	// Repositories
-	l.Info().
-		Msg("Instantiating repositories")
-
 	userRepository := repository.NewUserRepository(mongoDB)
-
-	// Services
-	l.Info().
-		Msg("Instantiating usecases")
-
 	userUsecase := usecase.NewUserUsecase(userRepository)
-
-	// Handlers
-	l.Info().
-		Msg("Instantiating handlers")
-
 	userHandler := handler.NewUserHandler(userUsecase)
-
-	// Router
-	l.Info().
-		Msg("Instantiating router")
 
 	e := echo.New()
 
@@ -58,9 +27,6 @@ func main() {
 	{
 		userHandler.Routes(apiGroup)
 	}
-
-	l.Info().
-		Msg("Starting server")
 
 	e.Logger.Fatal(e.Start(":8080"))
 }
